ceaa94f9bf76654b2980cf45e71a0ea8c15ec6cb
rlucas003 <rlucas003@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-03 17:10:55 +0000

    ajout src_autobus

---
+++ TD2/src_autobus/Autobus.java
@@ -0,0 +1,176 @@
+package tec;
+
+class Autobus {
+  static final byte VIDE   = 0;
+  static final byte DEBOUT = 1;
+  static final byte ASSIS  = 2;
+  static final byte PLEIN  = 4;
+  private byte status;
+  private int nb_debout;
+  private int nb_debout_left;
+  private int nb_assis;
+  private int nb_assis_left;
+  private int numero_arret;
+  private PassagerStandard[] passagers;
+
+
+
+  final Messages messages = new Messages();
+  Autobus() {
+    status = VIDE;
+  }
+
+  Autobus(byte init) {
+    status = init;
+  }
+
+  Autobus(int assis, int debout)
+  {
+	  nb_debout = debout;
+	  nb_assis = assis;
+	  passagers = new PassagerStandard[nb_debout+nb_assis];
+	  numero_arret = 0;
+
+	  nb_debout_left = debout;
+	  nb_assis_left = assis;
+	  
+	  if(nb_debout+nb_assis > 0)
+	  {
+		  status = VIDE;
+	  }
+	  else
+	  {
+		  status = PLEIN;
+	  }
+  }
+
+  public boolean aPlaceAssise() {
+	return status == ASSIS 
+      || status == VIDE;
+  }
+
+  public boolean aPlaceDebout() {
+    return status == DEBOUT 
+      || status == VIDE;
+  }
+
+  // Enregistrements des appels effectues par PassagerStandard.
+  public void monteeDemanderAssis(PassagerStandard p)
+  {
+	  if(p.estDehors())
+	  {
+		  if(aPlaceAssise())
+		  {
+			  p.changerEnAssis();
+			  nb_assis_left--;
+			  statusChanged();
+			  ajouterPassager(p);
+		  }
+	  }
+  }
+
+  public void monteeDemanderDebout(PassagerStandard p)
+  {
+	  if(p.estDehors())
+	  {
+		  if(aPlaceDebout())
+		  {
+			  p.changerEnDebout();
+			  nb_debout_left--;
+			  statusChanged();
+			  ajouterPassager(p);
+		  }
+	  }
+  }
+
+  public void arretDemanderDebout(PassagerStandard p)
+  {
+	  if(aPlaceAssise())
+	  {
+		  if(p.estAssis())
+		  {
+			p.changerEnDebout();
+			nb_assis_left++;
+			nb_debout_left--;
+			statusChanged();
+		  }
+	  }
+  }
+  
+  public void arretDemanderAssis(PassagerStandard p)
+  {
+	  if(aPlaceDebout())
+	  {
+		  if(p.estDebout())
+		  {
+			p.changerEnAssis();
+			nb_assis_left--;
+			nb_debout_left++;
+			statusChanged();
+		  }
+	  }
+  }
+
+  public void arretDemanderSortie(PassagerStandard p)
+  {
+	  if(!p.estDehors())
+	  {
+	  	p.changerEnDehors();
+		enleverPassager(p);
+	  }
+  }
+
+  private void statusChanged()
+  {
+	  if(nb_debout_left == 0)
+		  status &= DEBOUT^0xff;
+	  else
+	  {
+		  status &= PLEIN^0xff;
+		  status |= DEBOUT;
+	  }
+
+	  if(nb_assis_left == 0)
+		  status &= ASSIS^0xff;
+	  else
+	  {
+		  status &= PLEIN^0xff;
+		  status |= ASSIS;
+	  }
+
+	  if(nb_assis_left == nb_assis && nb_debout_left == nb_debout)
+		  status = VIDE;
+	  if(status == 0)
+		  status = PLEIN;
+  }
+
+  private void ajouterPassager(PassagerStandard p)
+  {
+	passagers[nb_assis_left+nb_debout_left-1] = p;
+  }
+
+  private void enleverPassager(PassagerStandard p)
+  {
+	  int decal = 0;
+
+	  for(int i=nb_assis+nb_debout-1;i>0;i--)
+	  {
+		  if(passagers[i] == p)
+			  decal = 1;
+
+		  if(decal == 1)
+			  passagers[i-1] = passagers[i];
+	  }
+	  passagers[0] = null;
+  }
+
+  // PassagerStandard n'utilise pas cette méthode.
+  public void allerArretSuivant()
+  { 
+	  for(int i=nb_assis+nb_debout-1;i>=0 && passagers[i] != null;i--)
+	  {
+		passagers[i].nouvelArret(this,numero_arret);
+	  }
+	  numero_arret++;
+  }
+}
---
+++ TD2/src_autobus/Jauge.java
@@ -0,0 +1,87 @@
+
+/**
+ * Réalisation d'une jauge avec un intervalle d'entiers.
+ * <p>
+ * Un objet Jauge définit un niveau dans un intervalle [0, vigieMax[. 
+ * Le niveau d'une jauge n'est pas limité aux valeurs dans l'intervalle.
+ * <p>
+ * L'état d'une jauge correspond à la position de son niveau par rapport à 
+ * l'intervalle [0, vigieMax[.
+ * Une jauge définit deux états : 
+ * <ul>
+ * <li>rouge niveau supérieur à l'intervalle,
+ * <li>vert niveau dans l'intervalle, 
+ * </ul>
+ * 
+ * @author georgy
+ * @since 2006-2016
+ */
+class Jauge { 
+  private int valeur;
+  private final int max;
+
+  /**
+   * Construit une instance en précisant la valeur de départ de la Jauge
+   * et l'intervalle de vigie.
+   * 
+   * @param vigieMax valeur maximale > 0 de l'intervalle de vigie.
+   * @param depart       valeur initiale du niveau.
+   */
+  public Jauge(int vigieMax, int depart) {
+    valeur = depart;
+    max = vigieMax;
+
+    if (vigieMax <=0) 
+      throw new IllegalArgumentException("vigieMax must be > 0");
+  }
+
+  /**
+   * Le niveau de la jauge est-il au-dessus de vigieMax ?
+   *
+   * @return vrai si niveau >=  vigieMax.
+   *
+   */
+  public boolean estRouge() {
+    return valeur >= max;
+  }
+
+  /**
+   * Le niveau de la jauge est-il dans l'intervalle ?
+   *
+   * @return vrai si niveau appartient à [0, vigieMax[.
+   *
+   */
+  public boolean estVert() {
+    return valeur >= 0 && !estRouge();
+  }
+
+  /**
+   * Incrémente le niveau d'une unité.
+   * Le niveau peut devenir supérieur à vigieMax.
+   */
+  public void incrementer() {
+    valeur++;
+  }
+
+  /**
+   * Décrémente le niveau d'une unité.
+   */
+  public void decrementer() {
+      valeur--;
+  }
+
+  /**
+   * Cette méthode est héritée de la classe {@link java.lang.Object}.
+   * Très utile pour le débogage, elle permet de fournir une 
+   * chaîne de caractères correspondant a l'état d'un objet.
+   * Mais, il faut adapter son code à chaque classe.
+   *
+   * @return la chaîne "<7 [0,23[>" si l'état d'une jauge 
+   * est vigieMax=23, niveau=7 ;
+   * 
+   */
+  @Override
+  public String toString() {
+    return "<" + valeur + " [0," + max + "[>"; 
+  }
+}
