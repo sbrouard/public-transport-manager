f5730a1f16e62890980258952b65e55290d51103
aduarte001 <aduarte001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-03 14:07:21 +0000

    classe minimale passager

---
+++ TD2/src_passager/PassagerStandard.java
@@ -0,0 +1,48 @@
+package tec;
+
+public class PassagerStandard() {
+
+    // Change la position d'un passager en assis
+    public void changerEnAssis() {
+    }
+
+    // Change la position d'un passager en debout
+    public void changerEnDebout() {
+    }
+
+    // Change la position d'un passager en dehors
+    public void changerEnDehors() {
+    }
+
+    // Le passager est-il assis dans l'autobus ?
+    public boolean estAssis() {
+	return;
+    }
+    
+    // Le passager est-il debout dans l'autobus ?
+    public boolean estDebout() {
+	return;
+    }
+
+    // Le passager est-il en dehors d'un autobus ?
+    public boolean estDehors() {
+	return;
+    }
+
+    // Fournit a un passager l'autobus auquel il peut accéder
+    public void monterDans(Autobus t) {
+    }
+
+    // Fournit le nom d'un passager
+    public String nom() {
+	return;
+    }
+    
+    // Indique au passager qu'il est arrivé à un nouvel arret
+    public void nouvelArret(Autobus bus, int numeroArret) {
+    }
+    
+    public String toString() {
+	return;
+    }
+}
