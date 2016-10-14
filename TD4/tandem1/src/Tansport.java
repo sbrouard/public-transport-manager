045b28a2e020f6be814a70ca2994343376ef25a8
aduarte001 <aduarte001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-14 10:22:46 +0000

    tandem 1

---
+++ TD4/tandem1/src/Tansport.java
@@ -0,0 +1,8 @@
+package tec;
+
+abstract class Transport {
+    public void allerArretSuivant();
+    
+    public boolean aPlaceAssise();
+    public boolean aPlaceDebout();
+}
---
+++ TD4/tandem1/src/Usager.java
@@ -0,0 +1,7 @@
+package tec;
+
+public abstract class Usager {
+    public String nom();
+    
+    public void monterDans(Vehicule b);
+}
