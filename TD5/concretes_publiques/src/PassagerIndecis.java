131e4c6dec64063bdf2f9d2116d2d7ff6eef433e
rlucas003 <rlucas003@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-21 10:18:16 +0000

    passagerindecis

--- TD5/concretes_publiques/src/PassagerIndecis.java
+++ TD5/concretes_publiques/src/PassagerIndecis.java
@@ -2,8 +2,21 @@ package tec;
 
 class PassagerIndecis extends PassagerAbstrait implements Usager{ 
 
-	void PassagerIndecis(String nom, int numArret)
+	public PassagerIndecis(String nom, int numArret)
 	{
 		super(nom,numArret);
+	}
+
+	void choixPlaceMontee(Vehicule v)
+	{
+		v.monteeDemanderDebout(this);	
+	}
+
+	void choixChangerPlace(Vehicule v, int arret)
+	{
+		if(estAssis())
+			v.arretDemanderDebout();
+		else if(estDebout())
+			v.arretDemanderAssis();
 	}
 }
