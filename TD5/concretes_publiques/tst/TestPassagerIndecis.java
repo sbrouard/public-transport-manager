7389888ae1f9ba1fdcf94c6b575e3e043053ce55
aduarte001 <aduarte001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-28 11:24:29 +0000

    tests

---
+++ TD5/concretes_publiques/tst/TestPassagerIndecis.java
@@ -0,0 +1,33 @@
+package tec;
+
+public class TestPassagerIndecis extends TestPassagerAbstrait {
+
+    protected PassagerAbstrait creerPassager(String nom, int destination) {
+	return new PassagerIndecis(nom, destination);
+    }
+
+    /* Interaction a la montee
+     * Trois cas
+     *  - des places assises et debout
+     *  - pas de place assise
+     *  - aucune place. 
+     */
+    public void testInteractionMontee() {
+	PassagerAbstrait p = creerPassager("yyy", 5);
+	
+	FauxVehicule faux = new FauxVehicule(FauxVehicule.VIDE);
+	p.monterDans(faux);
+	
+	assert "monteeDemanderAssis" == faux.messages.getLast() : "assis";    
+	
+	faux = new FauxVehicule(FauxVehicule.DEBOUT);
+	p.monterDans(faux);
+	
+	assert "monteeDemanderDebout" == faux.messages.getLast() : "debout";    
+	
+	faux = new FauxVehicule(FauxVehicule.PLEIN);
+	p.monterDans(faux);
+	
+	assert 0 == faux.messages.size() : "pas de place";        
+    }
+}
