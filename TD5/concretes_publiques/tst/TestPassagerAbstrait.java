852f02264fc5bd34cf958f34d1e92cf32955b472
aduarte001 <aduarte001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-10-28 13:41:56 +0000

    modification tests

--- TD5/concretes_publiques/tst/TestPassagerAbstrait.java
+++ TD5/concretes_publiques/tst/TestPassagerAbstrait.java
@@ -3,35 +3,6 @@ package tec;
 abstract public class TestPassagerAbstrait {
    
     abstract protected PassagerAbstrait creerPassager(String nom, int destination);
-
-    public static void main (String[] args) {
-	boolean estMisAssertion = false;
-	assert estMisAssertion = true;
-	
-	if (!estMisAssertion) {
-	    System.out.println("Execution impossible sans l'option -ea");
-	    return;
-	}
-
-	int nbTest = 0;
-	
-	//************ Verifier l'instanciation *************
-	System.out.print('.'); nbTest++;
-	new TestPassagerStandard().testInstanciation();
-	
-	//********* Verifier changement d'etat **************
-	System.out.print('.'); nbTest++;
-	new TestPassagerStandard().testGestionEtat();
-	
-	//********* Verifier les interactions  *************
-	System.out.print('.'); nbTest++;
-	new TestPassagerStandard().testInteractionMontee();
-	
-	System.out.print('.'); nbTest++;
-	new TestPassagerStandard().testInteractionArret();
-	
-	System.out.println("(" + nbTest + "):OK: " + "tec.PassagerStandard"); 
-    }
  
     /* Etat apres instanciation
      * Un seul cas 
@@ -66,23 +37,8 @@ abstract public class TestPassagerAbstrait {
 	assert false == p.estDebout();
 	assert false == p.estDehors();
     }  
-
-    /* Interaction a un arret
-     * Deux cas
-     *  - numero d'arret < a la destination
-     *  - numero d'arret >= a la destination 
-     */
-    public void testInteractionArret() {
-	PassagerAbstrait p = creerPassager("yyy", 5);
-	
-	FauxVehicule faux = new FauxVehicule(FauxVehicule.VIDE);
-	
-	p.nouvelArret(faux, 1);
-	assert 0 == faux.messages.size() : "pas a destination";
-	
-	p.nouvelArret(faux, 5);
-	assert "arretDemanderSortie" == faux.messages.getLast() : "destination";    
-    }
     
+    public abstract void testInteractionArret();
+
     public abstract void testInteractionMontee();
 }
--- TD5/concretes_publiques/tst/TestPassagerStresse.java
+++ TD5/concretes_publiques/tst/TestPassagerStresse.java
@@ -2,8 +2,55 @@ package tec;
 
 public class TestPassagerStresse extends TestPassagerAbstrait {
 
+    public static void main (String[] args) {
+	boolean estMisAssertion = false;
+	assert estMisAssertion = true;
+	
+	if (!estMisAssertion) {
+	    System.out.println("Execution impossible sans l'option -ea");
+	    return;
+	}
+
+	int nbTest = 0;
+	
+	//************ Verifier l'instanciation *************
+	System.out.print('.'); nbTest++;
+	new TestPassagerStresse().testInstanciation();
+	
+	//********* Verifier changement d'etat **************
+	System.out.print('.'); nbTest++;
+	new TestPassagerStresse().testGestionEtat();
+	
+	//********* Verifier les interactions  *************
+	System.out.print('.'); nbTest++;
+	new TestPassagerStresse().testInteractionMontee();
+	
+	System.out.print('.'); nbTest++;
+	new TestPassagerStresse().testInteractionArret();
+	
+	System.out.println("(" + nbTest + "):OK: " + "tec.PassagerStandard"); 
+    }
+
     protected PassagerAbstrait creerPassager(String nom, int destination) {
 	return new PassagerStresse(nom, destination);
+    }
+
+
+    /* Interaction a un arret
+     * Deux cas
+     *  - numero d'arret < a la destination
+     *  - numero d'arret >= a la destination 
+     */
+    public void testInteractionArret() {
+	PassagerAbstrait p = creerPassager("yyy", 5);
+	
+	FauxVehicule faux = new FauxVehicule(FauxVehicule.VIDE);
+	
+	p.nouvelArret(faux, 1);
+	assert 0 == faux.messages.size() : "pas a destination";
+	
+	p.nouvelArret(faux, 5);
+	assert "arretDemanderSortie" == faux.messages.getLast() : "destination";    
     }
 
     /* Interaction a la montee
@@ -18,7 +65,7 @@ public class TestPassagerStresse extends TestPassagerAbstrait {
 	FauxVehicule faux = new FauxVehicule(FauxVehicule.VIDE);
 	p.monterDans(faux);
 	
-	assert "monteeDemanderDebout" == faux.messages.getLast() : "debout";    
+	assert "monteeDemanderAssis" == faux.messages.getLast() : "assis";    
 	
 	faux = new FauxVehicule(FauxVehicule.DEBOUT);
 	p.monterDans(faux);
