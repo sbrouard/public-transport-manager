163e0ddd94cbe81d97b948a338cb9874d662da94
pmagne001 <pmagne001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-11-18 20:58:07 +0000

    cc

--- TD7/FabriqueTec/src/PassagerIndecis.java
+++
@@ -1,9 +0,0 @@
-package tec;
-
-class PassagerIndecis extends MonteeSportif implements Usager{ 
-
-	public PassagerIndecis(String nom, int numArret)
-	{
-	    super(nom,numArret,new ArretNerveux());
-	}
-}
--- TD7/FabriqueTec/src/PassagerStandard.java
+++
@@ -1,7 +0,0 @@
-package tec;
-
-class PassagerStandard extends MonteeRepos implements Usager{    
-    public PassagerStandard(String nom, int destination) {
-	super(nom,destination, new ArretCalme());
-    }        
-}
