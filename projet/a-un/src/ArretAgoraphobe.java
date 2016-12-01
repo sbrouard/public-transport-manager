80f668200f09905527557609dfabab0064c5add5
aduarte001 <aduarte001@660015e6-dc4c-493d-97fd-7633133eb65a>
2016-12-01 11:01:52 +0000

    ajout collecte fichier fonctionnelle + greffon

---
+++ projet/a-un/src/ArretAgoraphobe.java
@@ -0,0 +1,11 @@
+package tec;
+
+class ArretAgoraphobe implements ComportementArret{
+    
+    public void choixChangerPlace(int distDestination, Vehicule v, Passager P){
+	if ( !(v.aPlaceDebout()) && !(v.aPlaceAssise()) )
+	    v.arretDemanderSortie(P);
+    }
+
+
+}
---
+++ projet/a-un/src/Vehicule.java
@@ -0,0 +1,15 @@
+package tec;
+
+abstract class Vehicule{
+    abstract void monteeDemanderAssis(Passager p);
+    abstract void monteeDemanderDebout(Passager p);
+    
+    abstract void arretDemanderDebout(Passager p);
+    abstract void arretDemanderAssis(Passager p);
+    abstract void arretDemanderSortie(Passager p);
+    
+    abstract boolean aPlaceAssise();
+    abstract boolean aPlaceDebout();
+    abstract public void allerArretSuivant();
+    abstract public void allerArretSuivant(Greffon g);
+}
